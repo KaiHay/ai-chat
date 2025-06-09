01b8e409145c3fdebb0ecf0bf646ba447b3d7f4b
Kai Hay <kai@Kais-MacBook-Air.local>
2025-06-09 12:19:35 -0400

    chatting, limited response size

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,38 @@
+import { createOpenAI } from '@ai-sdk/openai';
+import { streamText } from 'ai';
+import { env } from 'process';
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+const openai = createOpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+})
+export async function POST(req: Request) {
+    const { messages } = await req.json();
+    console.log("THESE ARE mesages:", messages)
+
+    const result = streamText({
+        model: openai('gpt-4o-mini'),
+        system: 'You are a helpful assistant.',
+        messages,
+        maxTokens: 50,
+    });
+
+    return result.toDataStreamResponse({
+        sendReasoning: true, getErrorMessage: error => {
+            if (error == null) {
+                return 'unknown error';
+            }
+
+            if (typeof error === 'string') {
+                return error;
+            }
+
+            if (error instanceof Error) {
+                return error.message;
+            }
+
+            return JSON.stringify(error);
+        }
+    },);
+}
