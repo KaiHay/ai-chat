67c2498b02e1886caf7823580c23ef7c98879f16
Kai Hay <kai@Kais-MacBook-Air.local>
2025-06-09 16:00:58 -0400

    chat persists on local files, not on db yet

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,5 +1,6 @@
 import { createOpenAI } from '@ai-sdk/openai';
-import { streamText, type Message } from 'ai';
+import { appendClientMessage, appendResponseMessages, streamText, type Message } from 'ai';
+import { loadChat, saveChat } from '../../../tools/chat-store'
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
@@ -7,16 +8,31 @@ const openai = createOpenAI({
     apiKey: process.env.OPENAI_API_KEY,
 })
 export async function POST(req: Request) {
-    const { messages }: { messages: Message[] } = await req.json() as { messages: Message[] };
+    const { message, id }: { message: Message, id: string } = await req.json() as { message: Message, id: string };
     //console.log("THESE ARE mesages:", messages)
-
+    const prevMessages = await loadChat(id)
+    const messages = appendClientMessage({
+        messages: prevMessages,
+        message,
+    })
     const result = streamText({
         model: openai('gpt-4o-mini'),
         system: 'You are a helpful assistant.',
         messages,
         maxTokens: 50,
+        async onFinish({ response }) {
+            await saveChat({
+                id,
+                messages: appendResponseMessages({
+                    messages,
+                    responseMessages: response.messages,
+                })
+            })
+        }
     });
+    result.consumeStream(); 
 
+    
     return result.toDataStreamResponse({
         sendReasoning: true, getErrorMessage: error => {
             if (error == null) {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
 // import Link from "next/link";
 // import { LatestPost } from "~/app/_components/post";
 // import { api, HydrateClient } from "~/trpc/server";
-import { Chat } from "~/app/_components/chat/page";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   // const hello = await api.post.hello({ text: "from tRPC" });
 
   // void api.post.getLatest.prefetch();
-
+  redirect(`/chat`)
   return (
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#7d1630] to-[#a4a91b] text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <Chat />
+          {/* <Chat /> */}
         </div>
       </main>
   );
