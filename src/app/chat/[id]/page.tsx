29a9ee81da867244dc289187fccb9175c7e4f096
Kai Hay <kai@Kais-MacBook-Air.local>
2025-06-12 17:54:55 -0400

    added auth to all chats and fixed persistence errors

--- src/app/chat/[id]/page.tsx
+++ src/app/chat/[id]/page.tsx
@@ -1,10 +1,28 @@
 import type { Message } from 'ai';
-import { loadChat } from '../../../tools/chat-store';
+import { loadChat, loadMessages } from '../../../tools/chat-store';
 import Chat from '../../ui/chat';
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
+import { auth } from '~/lib/auth';
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
+    const session = await auth.api.getSession({
+        headers: await headers()
+    })
+    if (!session) redirect(`/chat`)
+
     const { id } = await props.params; // get the chat ID from the URL
-    const messages = await loadChat(id); // load the chat messages
+
+    const currChat = await loadChat(id)
+    console.log('CurrentChat: ', currChat)
+    if (!currChat[0]) redirect('/chat')
+    if (session.user.id != currChat[0].userId) {
+        console.log('Not your chat')
+        redirect('/chat')
+    }
+    const messages = await loadMessages(currChat[0].id); // load the chat messages
+
+
     const prevMessages: Message[] = messages.map(record => ({
         role: record.role as 'data' | 'user' | 'assistant' | 'system',
         content: record.content ?? '', // Convert null to empty string
---
+++ src/app/ui/chatList.tsx
@@ -0,0 +1,13 @@
+
+'use client'
+import { useChat, type Message } from '@ai-sdk/react'
+import clsx from 'clsx'
+
+export default function ListChats() {
+ 
+    return (
+        <div>
+            
+        </div>
+    )
+}
