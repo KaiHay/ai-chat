f469990472760150fe43444cbc321e1a6414402b
Kai Hay <kai@mac.mynetworksettings.com>
2025-06-13 11:49:46 -0400

    moving some methods to trpc

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,14 +1,17 @@
 import { redirect } from 'next/navigation'
-import { createChat } from '../../tools/chat-store'
+// import { createChat } from '../../tools/chat-store'
 import { auth } from '~/lib/auth'
 import { headers } from 'next/headers'
+import { api } from '~/trpc/server';
+
 export default async function Page() {
     const session = await auth.api.getSession({
         headers: await headers()
     })
     if (!session) redirect(`/login`)
     console.log("sessionId: ", session.user.name)
-    const id = await createChat(session.user.id)
+    // const id = await createChat(session.user.id)
+    const id = await api.chat.createNewChats({ userId: session.user.id })
     console.log('gonna redirect to: ', id.id)
     redirect(`/chat/${id.id}`)
 }
